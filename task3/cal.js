8631dc729c747e4bf22e81d1ddb1386d9849a810
meer-wasiq <meerwasiq19@gmail.com>
2023-09-11 12:00:10 +0530

    first commit

---
+++ task3/cal.js
@@ -0,0 +1,21 @@
+let displayValue = "";
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById("display").textContent = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = "";
+    document.getElementById("display").textContent = "0";
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue);
+        document.getElementById("display").textContent = displayValue;
+    } catch (error) {
+        document.getElementById("display").textContent = "Error";
+        displayValue = "";
+    }
+}
